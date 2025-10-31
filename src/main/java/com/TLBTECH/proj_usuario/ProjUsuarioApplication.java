4949c78bcca856ccd9c3552242c54f1e99e4f742
Taciano <taciolb@gmail.com>
2025-10-31 20:25:02 -0300

    commit inicial

---
+++ src/main/java/com/TLBTECH/proj_usuario/ProjUsuarioApplication.java
@@ -0,0 +1,13 @@
+package com.TLBTECH.proj_usuario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjUsuarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjUsuarioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/TLBTECH/proj_usuario/ProjUsuarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TLBTECH.proj_usuario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjUsuarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
